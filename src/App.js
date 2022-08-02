071d778d42a8aafff11e3dd983dae7205765d472
benekker <benekker1@gmail.com>
2022-08-02 14:41:19 -0500

    Renamed CreatePost component + Created NavBar component and added to app file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, { useState } from 'react';
+import NavBar from './Components/NavBar/NavBar';
 
 
 function App() {
+
+  const [posts, setPosts] = useState([])
+
+
+
+
+
+
+
+
+
   return (
     <div>
-     
+      <NavBar />
+      
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import './NavBar.css';
+
+const NavBar = (props) =>{
+    return (
+        <div className="container-fluid">
+                <h1 style={{'margin': '10px'}}>Social
+                <small className="text-muted">Feed</small>
+                </h1>
+        </div>
+    );
+}
+export default NavBar;
